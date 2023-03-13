0c1f6da94a03f1c48c8dc34540f3d2862719b519
SVA2021 <sofronov.vit@gmail.com>
2023-03-13 13:19:14 +0400

    add best movie service

--- src/app/services/best-movie.service.ts
+++ src/app/services/best-movie.service.ts
@@ -1,30 +1,33 @@
 import {Injectable} from '@angular/core';
 import {movieT} from "../types/types";
+import {Subject} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class BestMovieService {
 
-  localStorageKey = 'myBestMovie'
-
-  get movie(): movieT | null {
-    return this._movie;
-  }
-
-  private _movie: movieT | null
+  private localStorageKey = 'myBestMovie'
+  bestMovie$: Subject<movieT | null>
+  activeBestMovie: movieT | null
 
   constructor() {
-    this._movie = localStorage.getItem(this.localStorageKey)
-      ? JSON.parse(localStorage.getItem(this.localStorageKey) ?? '')
-      : null;
+    this.activeBestMovie = null;
+    this.bestMovie$ = new Subject<movieT | null>();
+    this.bestMovie$.next(
+      localStorage.getItem(this.localStorageKey)
+        ? JSON.parse(localStorage.getItem(this.localStorageKey) ?? '')
+        : null
+    )
+    this.bestMovie$.subscribe(movie => this.activeBestMovie = movie);
   }
 
-  setBestMovie(film: movieT | null) {
-    this._movie = film;
-    if (film) {
+  setBestMovie(film: movieT) {
+    if (film.id !== this.activeBestMovie?.id || !this.activeBestMovie) {
+      this.bestMovie$.next(film);
       localStorage.setItem(this.localStorageKey, JSON.stringify(film));
     } else {
+      this.bestMovie$.next(null);
       localStorage.clear();
     }
   }
