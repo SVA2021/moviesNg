4e2d274352e202166ec926e0055603c82f66ebef
SVA2021 <sofronov.vit@gmail.com>
2023-03-13 18:42:18 +0400

    code cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {
-  BestMovieComponent, DetailedMovieComponent,
+  BestMovieComponent,
+  DetailedMovieComponent,
   MovieCardComponent,
   MoviesListComponent,
 } from './components';
@@ -16,6 +16,8 @@ import {MatIconModule} from "@angular/material/icon";
 import {MatDialogModule} from "@angular/material/dialog";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatSelectModule} from "@angular/material/select";
+import {MatInputModule} from "@angular/material/input";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -36,7 +38,8 @@ import {MatSelectModule} from "@angular/material/select";
     MatDialogModule,
     MatFormFieldModule,
     MatSelectModule,
-
+    MatInputModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
